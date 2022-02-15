e4e3dd8b377889f168824c38803f81c50f706447
Your Name <keisi815keisi@gmail.com>
2022-02-15 20:28:04 +0900

    first commit

---
+++ TennisAnalysis/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  singnUpViewController.swift
+//  TennisAnalysis
+//
+//  Created by 市田圭司 on 2021/11/25.
+//
+
+import UIKit
+
+class singnUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TennisAnalysis/SyosaiTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SyosaiTableViewCell.swift
+//  TennisAnalysis
+//
+//  Created by 市田圭司 on 2021/12/01.
+//
+
+import UIKit
+
+class SyosaiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
